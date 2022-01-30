47f9bc1c914963797b92406f03f7db4983303f61
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-01-30 18:08:09 +0900

    バリデーションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'haikus#index'
   resources :users, only: [:edit, :update, :show]
+  get '/users', to: 'haikus#index'
   resources :haikus, only: [:index]
 end

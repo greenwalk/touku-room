235d72b6d4ae5f16d03fa62135e8dc8a64d02b89
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-02-01 01:04:43 +0900

    バリデーションとアソシエーション

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -5,6 +5,7 @@ class Theme < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :season_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :season_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status, presence: true 
   validates :image, presence: true
 end

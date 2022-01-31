49e17462d6c3e35d445fe0bf8da2a968bdfb0381
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-01-31 22:41:36 +0900

    モデルの作成

---
+++ app/models/theme.rb
@@ -0,0 +1,10 @@
+class Theme < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :season
+
+  belongs_to :user
+  has_one_attached :image
+
+  validates :season_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :image, presence: true
+end

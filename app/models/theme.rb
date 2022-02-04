f68cf76e3afec5d9ed7b5761dcdfc94798a244a8
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-02-04 11:58:54 +0900

    Themeモデルのstatusカラムをenumによって定義

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -8,4 +8,6 @@ class Theme < ApplicationRecord
   validates :season_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :status, presence: true 
   validates :image, presence: true
+
+  enum status: { set: 0, used: 1 }
 end

f4863db1331417e8e1ec837dc84179fe0c14bfa0
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-02-05 14:51:31 +0900

    アソシエーションとバリデーション

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,5 +1,9 @@
 class Field < ApplicationRecord
   belongs_to :theme
+  has_many :haikus
 
+  validates :status, presence: true
+  validates :theme, presence: true
+  
   enum status: { touku: 0, voting: 1, finished: 2 }
 end
--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -1,2 +1,7 @@
 class Haiku < ApplicationRecord
+  belongs_to :user
+  belongs_to :field
+
+  validates :content, presence: true
+  validates :content_sub, presence: true, format: { with: /\A[ぁ-んー－]+\z/, message: "「ひらがな」のみ入力可能です" }
 end

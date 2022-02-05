cec72941557dd822ce6eb42b2452d98e8690e187
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-02-05 19:01:19 +0900

    バリデーションの追記

--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -2,6 +2,6 @@ class Haiku < ApplicationRecord
   belongs_to :user
   belongs_to :field
 
-  validates :content, presence: true
-  validates :content_sub, presence: true, format: { with: /\A[ぁ-んー－]+\z/, message: "「ひらがな」のみ入力可能です" }
+  validates :content, presence: true, length:{ minimum: 5, maximum: 25 }
+  validates :content_sub, presence: true, format: { with: /\A[ぁ-んー－]+\z/, message: "「ひらがな」のみ入力可能です" }, length:{ minimum: 5, maximum: 25}
 end

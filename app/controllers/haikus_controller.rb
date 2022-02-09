a876e72271c53874714e62c07f4af2f43a35f9b3
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-02-09 13:27:47 +0900

    コントローラーの設定

--- app/controllers/haikus_controller.rb
+++ app/controllers/haikus_controller.rb
@@ -1,28 +1,32 @@
 class HaikusController < ApplicationController
-  before_action :haiku_set, only: [:update]
-  before_action :authenticate_user!, only: [:new]
+  before_action :haiku_set, only: [:edit, :update]
+  before_action :field_set, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_top, only: [:edit]
+  before_action :move_to_edit, only: [:new]
 
   def new
-    @field = Field.find(params[:field_id])
-    @haiku = Haiku.new
+    unless Haiku.exists?(user_id: current_user.id, field_id: @field.id)
+      @haiku = Haiku.new
+    end
   end
 
   def create
     @haiku = Haiku.new(haiku_params)
     if @haiku.save
-      redirect_to root_path
+      redirect_to "/fields/#{@haiku.field.id}/haikus/#{@haiku.id}/edit"
     else
       render :new
     end
   end
 
   def edit
+
   end
 
   def update
     if @haiku.update(haiku_params)
-      redirect_to edit_haiku_path
+      redirect_to "/fields/#{@haiku.field.id}/haikus/#{@haiku.id}/edit"
     else
       render :edit
     end
@@ -37,9 +41,19 @@ class HaikusController < ApplicationController
     @haiku = Haiku.find(params[:id])
   end
 
+  def field_set
+    @field = Field.find(params[:field_id])
+  end
+
   def move_to_top
     unless current_user.id == @haiku.user_id
       redirect_to root_path
+    end
+  end
+
+  def move_to_edit
+    if Haiku.exists?(user_id: current_user.id, field_id: @field.id)
+      redirect_to "/fields/#{@haiku.field.id}/haikus/#{@haiku.id}/edit"
     end
   end
 

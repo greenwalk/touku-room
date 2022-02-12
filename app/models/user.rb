396fb840ccc34f6e986f815872a04365b3cdb5ef
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-02-12 17:52:54 +0900

    バリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ApplicationRecord
 
   has_many :themes
   has_many :haikus
+  has_many :votes
   
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :name, presence: true
+
+  def already_voted?(haiku)
+    self.votes.exists?(haiku_id: haiku.id)
+  end
 end

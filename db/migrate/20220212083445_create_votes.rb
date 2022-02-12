a13d343c4e0f1941578ab786ff13fa1436344282
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-02-12 17:52:28 +0900

    マイグレーション

--- db/migrate/20220212083445_create_votes.rb
+++ db/migrate/20220212083445_create_votes.rb
@@ -1,7 +1,8 @@
 class CreateVotes < ActiveRecord::Migration[6.0]
   def change
     create_table :votes do |t|
-
+      t.references :user , null: false, foreign_key: true
+      t.references :haiku, null: false, foreign_key: true
       t.timestamps
     end
   end

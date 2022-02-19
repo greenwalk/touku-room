9da44b9b874128b9e87cfa973e27bf9de45bf371
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-02-19 15:32:22 +0900

    schedule.rbの設定

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,21 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+# 出力先のログファイルの指定
+set :output, 'log/cron.log'
+# cronを実行する環境変数
+rails_env = ENV['RAILS_ENV'] || :development
+# cronを実行する環境変数をセット
+set :environment, rails_env
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
+# 日本のタイムゾーン
+def jst(time)
+  Time.zone = 'Asia/Tokyo'
+  Time.zone.parse(time).localtime($system_utc_offset)
+end
+
+# every 1.day, at:jst('3:22 pm') do
+#   rake 'change_status:chage_field_status'
 # end
 
-# Learn more: http://github.com/javan/whenever
+# every 1.day, at: jst('3:25 pm') do
+#   rake 'change_status:create_new_field'
+# end

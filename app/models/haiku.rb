62951531aacca06b7c87dfa55dd9e115de0bfff1
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-03-12 23:10:14 +0900

    エラーメッセージ修正

--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -4,6 +4,6 @@ class Haiku < ApplicationRecord
   has_many :votes
 
   validates :content, presence: true, length: { minimum: 5, maximum: 25 }
-  validates :content_sub, presence: true, format: { with: /\A[ぁ-んー－]+\z/, message: '「ひらがな」のみ入力可能です' },
+  validates :content_sub, presence: true, format: { with: /\A[ぁ-んー－]+\z/, message: 'は「ひらがな」のみ入力可能です（スペースも不可）' },
                           length: { minimum: 5, maximum: 25 }
 end

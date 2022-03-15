f91660c19c2d1f377956782422dd99b5b8ac028a
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-03-16 02:00:36 +0900

    画像をつけないで保存ボタンを押した時のエラーを解消

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -6,14 +6,18 @@ class Theme < ApplicationRecord
   has_one_attached :image
   has_one :field
 
-  validates :season_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :season_id, numericality: { other_than: 1, message: "を選択してください" }
   validates :status, presence: true
   validates :image, presence: true
-  validate :image_content_type
+  validate :image_content_type, if: :was_attached?
 
   def image_content_type
     extension = ['image/png', 'image/jpg', 'image/jpeg']
     errors.add(:image, "に使用可能な拡張子は .png/.jpg/.jpeg のみです") unless image.content_type.in?(extension)
+  end
+
+  def was_attached?
+    self.image.attached?
   end
 
   enum status: { set: 0, used: 1 }
